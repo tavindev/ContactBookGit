e67b134d5ae44743c3662fa9cf108515315790c0
tavindev <gvwchevrand@gmail.com>
2024-09-17 14:22:34 +0100

    fix(Main): typos and EP command
    Fix typos on command outputs and remove the number of contact checks on EP command

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,20 @@ import contactBook.ContactBook;
 
 import java.util.Scanner;
 
-
 public class Main {
-    //Constantes que definem os comandos
-    public static final String ADD_CONTACT      = "AC";
-    public static final String REMOVE_CONTACT   = "RC";
-    public static final String GET_PHONE        = "GP";
-    public static final String GET_EMAIL        = "GE";
-    public static final String SET_PHONE        = "SP";
-    public static final String SET_EMAIL        = "SE";
-    public static final String LIST_CONTACTS    = "LC";
-    public static final String QUIT             = "Q";
-    public static final String GET_NUMER        = "GN";
-    public static final String EVALUATE_PHONES  = "EP";
+    // Constantes que definem os comandos
+    public static final String ADD_CONTACT = "AC";
+    public static final String REMOVE_CONTACT = "RC";
+    public static final String GET_PHONE = "GP";
+    public static final String GET_EMAIL = "GE";
+    public static final String SET_PHONE = "SP";
+    public static final String SET_EMAIL = "SE";
+    public static final String LIST_CONTACTS = "LC";
+    public static final String QUIT = "Q";
+    public static final String GET_NUMER = "GN";
+    public static final String EVALUATE_PHONES = "EP";
 
-    //Constantes que definem as mensagens para o utilizador
+    // Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
     public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";
     public static final String CONTACT_ADDED = "contactBook.Contact added.";
@@ -27,39 +26,39 @@ public class Main {
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
     public static final String PHONE_NOT_EXISTS = "Phone number does not exist.";
-    public static final String SHARE_PHONE_NUMBERS = "There are contacts that sahre the same phone number.";
-    public static final String DIFFERENT_PHOE_NUMBERS = "All contacts have different phone numbers.";
+    public static final String SHARE_PHONE_NUMBERS = "There are contacts that share phone numbers.";
+    public static final String DIFFERENT_PHONE_NUMBERS = "All contacts have different phone numbers.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         ContactBook cBook = new ContactBook();
         String comm = getCommand(in);
 
-        while (!comm.equals(QUIT)){
+        while (!comm.equals(QUIT)) {
             switch (comm) {
                 case ADD_CONTACT:
-                    addContact(in,cBook);
+                    addContact(in, cBook);
                     break;
                 case REMOVE_CONTACT:
-                    deleteContact(in,cBook);
+                    deleteContact(in, cBook);
                     break;
                 case GET_PHONE:
-                    getPhone(in,cBook);
+                    getPhone(in, cBook);
                     break;
                 case GET_EMAIL:
-                    getEmail(in,cBook);
+                    getEmail(in, cBook);
                     break;
                 case SET_PHONE:
-                    setPhone(in,cBook);
+                    setPhone(in, cBook);
                     break;
                 case SET_EMAIL:
-                    setEmail(in,cBook);
+                    setEmail(in, cBook);
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
                     break;
                 case GET_NUMER:
-                    getNumber(in,cBook);
+                    getNumber(in, cBook);
                     break;
                 case EVALUATE_PHONES:
                     evaluatePhones(cBook);
@@ -87,13 +86,14 @@ public class Main {
         int phone;
 
         name = in.nextLine();
-        phone = in.nextInt(); in.nextLine();
+        phone = in.nextInt();
+        in.nextLine();
         email = in.nextLine();
         if (!cBook.hasContact(name)) {
             cBook.addContact(name, phone, email);
             System.out.println(CONTACT_ADDED);
-        }
-        else System.out.println(CONTACT_EXISTS);
+        } else
+            System.out.println(CONTACT_EXISTS);
     }
 
     private static void deleteContact(Scanner in, ContactBook cBook) {
@@ -102,8 +102,8 @@ public class Main {
         if (cBook.hasContact(name)) {
             cBook.deleteContact(name);
             System.out.println(CONTACT_REMOVED);
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else
+            System.out.println(NAME_NOT_EXIST);
     }
 
     private static void getPhone(Scanner in, ContactBook cBook) {
@@ -111,8 +111,8 @@ public class Main {
         name = in.nextLine();
         if (cBook.hasContact(name)) {
             System.out.println(cBook.getPhone(name));
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else
+            System.out.println(NAME_NOT_EXIST);
     }
 
     private static void getEmail(Scanner in, ContactBook cBook) {
@@ -120,20 +120,21 @@ public class Main {
         name = in.nextLine();
         if (cBook.hasContact(name)) {
             System.out.println(cBook.getEmail(name));
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else
+            System.out.println(NAME_NOT_EXIST);
     }
 
     private static void setPhone(Scanner in, ContactBook cBook) {
         String name;
         int phone;
         name = in.nextLine();
-        phone = in.nextInt(); in.nextLine();
+        phone = in.nextInt();
+        in.nextLine();
         if (cBook.hasContact(name)) {
-            cBook.setPhone(name,phone);
+            cBook.setPhone(name, phone);
             System.out.println(CONTACT_UPDATED);
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else
+            System.out.println(NAME_NOT_EXIST);
     }
 
     private static void setEmail(Scanner in, ContactBook cBook) {
@@ -142,39 +143,37 @@ public class Main {
         name = in.nextLine();
         email = in.nextLine();
         if (cBook.hasContact(name)) {
-            cBook.setEmail(name,email);
+            cBook.setEmail(name, email);
             System.out.println(CONTACT_UPDATED);
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else
+            System.out.println(NAME_NOT_EXIST);
     }
 
     private static void listAllContacts(ContactBook cBook) {
         if (cBook.getNumberOfContacts() != 0) {
             cBook.initializeIterator();
-            while( cBook.hasNext() ) {
+            while (cBook.hasNext()) {
                 Contact c = cBook.next();
                 System.out.println(c.getName() + "; " + c.getEmail() + "; " + c.getPhone());
             }
-        }
-        else System.out.println(BOOK_EMPTY);
+        } else
+            System.out.println(BOOK_EMPTY);
     }
 
     private static void getNumber(Scanner in, ContactBook cBook) {
         int phone;
-        phone = in.nextInt(); in.nextLine();
+        phone = in.nextInt();
+        in.nextLine();
         if (cBook.hasContact(phone)) {
             System.out.println(cBook.getName(phone));
-        }
-        else System.out.println(PHONE_NOT_EXISTS);
+        } else
+            System.out.println(PHONE_NOT_EXISTS);
     }
 
     private static void evaluatePhones(ContactBook cBook) {
-        if (cBook.getNumberOfContacts() != 0) {
-            if (cBook.checkForRepeatedNumbers())
-                System.out.println(SHARE_PHONE_NUMBERS);
-            else
-                System.out.println(DIFFERENT_PHOE_NUMBERS);
-        }
-        else System.out.println(BOOK_EMPTY);
+        if (cBook.checkForRepeatedNumbers())
+            System.out.println(SHARE_PHONE_NUMBERS);
+        else
+            System.out.println(DIFFERENT_PHONE_NUMBERS);
     }
 }
